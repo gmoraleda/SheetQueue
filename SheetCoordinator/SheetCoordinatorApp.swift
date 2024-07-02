c2f1cb2af5a0a742052abc62cd1d91d9efc94dc9
Guillermo Moraleda <guillermo.moraleda@audi.de>
2024-07-02 08:23:41 +0200

    Initial Commit

---
+++ SheetCoordinator/SheetCoordinatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  SheetCoordinatorApp.swift
+//  SheetCoordinator
+//
+//  Created by Moraleda, Guillermo (ABI) on 02.07.24.
+//
+
+import SwiftUI
+
+@main
+struct SheetCoordinatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
